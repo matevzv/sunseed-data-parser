e5817d9ac11c0de3bc52ae14b9c952f56e3f4d20
Matevz Vucnik <vucnikm@gmail.com>
2016-08-12 17:42:17 +0200

    fix indentation

--- pmc.js
+++ pmc.js
@@ -2,16 +2,16 @@ var Port = require("serialport");
 var parser = require('./sunseed-parser');
 
 var port = new Port("/dev/ttyMFD1", {
-	baudRate: 115200
+  baudRate: 115200
 });
 
 port.on('data', function(data) {
-	parser.pmc(data, function(err, parsed_data){
-		if (err) {
-			console.log(err);
-		}
-		else {
-			console.log(parsed_data);
-	  }
-	});
+  parser.pmc(data, function(err, parsed_data){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(parsed_data);
+    }
+  });
 });
