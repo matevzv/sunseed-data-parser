5c9f607d303412e0d728ca6540e933abefe0ef02
Matevz Vucnik <vucnikm@gmail.com>
2016-08-18 09:41:12 +0200

    fix parser test

--- sunseed-parser-test.js
+++ sunseed-parser-test.js
@@ -1,15 +1,23 @@
 var parser = require('./sunseed-parser');
 
+// simulate PMC an SPM data
+var pmc_data_length = 50;
+var spm_data_length = 14;
+
 var data_pmc = ["PMC"]
 var data_spm = ["SPM"]
 
-for (var i = 0; i < 50; i++) {
+for (var i = 0; i < pmc_data_length; i++) {
   data_pmc.push(i);
-  if (i < 14) {
+  if (i < spm_data_length) {
     data_spm.push(i);
   }
 }
 
+data_pmc.push(data_pmc.pop() + "\n");
+data_spm.push(data_spm.pop() + "\n");
+
+// test PMC and SPM parser functions
 parser.pmc(data_pmc, function (err, parsed_data) {
   if (err) {
     console.log(err);
