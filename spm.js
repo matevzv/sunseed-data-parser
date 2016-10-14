04162bcec911958fc2ed8ee540ed65d43a9b0f21
Matevz Vucnik <vucnikm@gmail.com>
2016-10-14 17:01:15 +0200

    add mqtt

--- spm.js
+++ spm.js
@@ -1,20 +1,39 @@
 #!/usr/bin/env node
 
-var Port = require('serialport');
-var parser = require('sunseed-parser');
+var fs = require('fs');
+var mqtt = require('mqtt')
+var SerialPort = require('serialport');
+var sunseed_parser = require('sunseed-parser');
 
-var port = new Port('/dev/ttyMFD1', {
-  baudRate: 115200,
-  parity: 'odd'
-});
+var client  = mqtt.connect('mqtt://10.122.248.42')
 
-port.on('data', function (data) {
-  parser.spm(data, function (err, parsed_data) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      setTimeout(function() { process.exit(); }, 1000);
-    }
-  });
+fs.readFile('/etc/machine-id', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var machine_id = data.toString().slice(0, -1);
+  var topic = "spm/" + machine_id;
+  client.on('connect', function () {
+    client.subscribe(topic);
+    fs.appendFile("/var/log/spm-error.log",
+      err+"\nData: "+data+"\n"+"----------"+"\n");
+
+    var port = new SerialPort('/dev/ttyMFD1', {
+      baudRate: 115200,
+      parity: 'odd',
+      parser: SerialPort.parsers.readline()
+    });
+
+    port.on('data', function (data) {
+      sunseed_parser.spm(data, function (err, parsed_data) {
+        if (err) {
+          fs.appendFile("/var/log/spm-error.log",
+            err+"\nData: "+data+"\n"+"----------"+"\n");
+        }
+        else {
+          client.publish(topic, parsed_data);
+        }
+      });
+    });
+  })
 });
