240ce26b19cfd155f5bac69b3ac58f5a2598a570
Matevz Vucnik <vucnikm@gmail.com>
2016-11-10 10:37:43 +0100

    refactor the code to be more javascript

--- spm.js
+++ spm.js
@@ -6,78 +6,64 @@ var SerialPort = require('serialport');
 var sunseed_parser = require('sunseed-parser');
 
 // commandline args
-if (process.argv.length > 2) {
+var process_cli = function (callback) {
+  var limit = false;
+
   process.argv.forEach(function (val, index) {
     if (val == "-h" || val == "--help") {
       console.log("Usage: spm [OPTIONS] [arg...]\n");
       console.log("The Sunseed data parser.\n");
       console.log("Options:\n");
-      console.log("-h, --help            Print usage");
-      console.log("-f, --freq            Measurement frequency high or low");
-      console.log("                      default: high");
+      console.log("-h, --help          Print usage");
+      console.log("-l, --limit         Limit measurement period to 1 second");
       process.exit();
     }
-    else if (val == "-f" || val == "--freq") {
-      freq = process.argv[index+1];
-      if (freq == "low") {
-        console.log("Measurement frequency set to " + freq + ".");
-        slow = true;
-      }
-      else if (freq == "high") {
-        console.log("Measurement frequency set to " + freq + ".");
-        slow = false;
-      }
-      else {
-        console.log("Incorrect measurement frequency setting! See 'spm --help'.");
-        process.exit(1);
-      }
+    else if (val == "-l" || val == "--limit") {
+      console.log("Measurement period limited to 1 second.");
+      limit = true;
     }
-    else if (index >= 2 && val !== "low" && val !== "high") {
-      console.log("spm: '" + val + "' is not a spm command. See 'spm --help'.");
-      process.exit(1);
-    }
-    else if (index == 2 && (val == "low" || val == "high")) {
+    else if (index > 1) {
       console.log("spm: '" + val + "' is not a spm command. See 'spm --help'.");
       process.exit(1);
     }
   });
+
+  callback(limit);
 }
-else {
-  slow = false;
-}
 
-fs.readFile('/etc/machine-id', function (err, file_data) {
-  if (err) {
-    return console.log(err);
-  }
-  var machine_id = file_data.toString().slice(0, -1);
-  var topic = "spm/" + machine_id;
+process_cli(function (limit) {
+  fs.readFile('/etc/machine-id', function (err, file_data) {
+    if (err) {
+      return console.log(err);
+    }
+    var machine_id = file_data.toString().slice(0, -1);
+    var topic = "spm/" + machine_id;
 
-  var client  = mqtt.connect('mqtt://10.122.248.42');
-  client.on('connect', function () {
-    var port = new SerialPort('/dev/ttyMFD1', {
-      baudRate: 115200,
-      parity: 'odd',
-      parser: SerialPort.parsers.readline()
-    });
+    var client  = mqtt.connect('mqtt://10.122.248.42');
+    client.on('connect', function () {
+      var port = new SerialPort('/dev/ttyMFD1', {
+        baudRate: 115200,
+        parity: 'odd',
+        parser: SerialPort.parsers.readline()
+      });
 
-    port.on('data', function (serial_data) {
-      sunseed_parser.spm(serial_data, machine_id, function (err, parsed_data) {
-        if (err) {
-          console.log(err + " Data: " + serial_data);
-        }
-        else {
-          if (typeof slow !== 'undefined') {
-            if (slow) {
+      port.on('data', function (serial_data) {
+        sunseed_parser.spm(serial_data, machine_id, function (err, parsed_data) {
+          if (err) {
+            console.log(err + " Data: " + serial_data);
+          }
+          else {
+            if (limit) {
               if (parsed_data.indexOf('"report_n":49') !== -1) {
                 client.publish(topic, parsed_data);
               }
-            } else {
+            }
+            else {
               client.publish(topic, parsed_data);
             }
           }
-        }
+        });
       });
-    });
-  })
+    })
+  });
 });
--- sunseed-parser-test.js
+++ sunseed-parser-test.js
@@ -4,44 +4,29 @@ var fs = require('fs');
 var sunseed_parser = require('./sunseed-parser');
 
 // commandline args
-if (process.argv.length > 2) {
+var process_cli = function (callback) {
+  var limit = false;
+
   process.argv.forEach(function (val, index) {
     if (val == "-h" || val == "--help") {
       console.log("Usage: spm [OPTIONS] [arg...]\n");
       console.log("The Sunseed data parser.\n");
       console.log("Options:\n");
-      console.log("-h, --help            Print usage");
-      console.log("-f, --freq            Measurement frequency high or low");
-      console.log("                      default: high");
+      console.log("-h, --help          Print usage");
+      console.log("-l, --limit         Limit measurement period to 1 second");
       process.exit();
     }
-    else if (val == "-f" || val == "--freq") {
-      freq = process.argv[index+1];
-      if (freq == "low") {
-        console.log("Measurement frequency set to " + freq + ".");
-        slow = true;
-      }
-      else if (freq == "high") {
-        console.log("Measurement frequency set to " + freq + ".");
-        slow = false;
-      }
-      else {
-        console.log("Incorrect measurement frequency setting! See 'spm --help'.");
-        process.exit(1);
-      }
+    else if (val == "-l" || val == "--limit") {
+      console.log("Measurement period limited to 1 second.");
+      limit = true;
     }
-    else if (index >= 2 && val !== "low" && val !== "high") {
-      console.log("spm: '" + val + "' is not a spm command. See 'spm --help'.");
-      process.exit(1);
-    }
-    else if (index == 2 && (val == "low" || val == "high")) {
+    else if (index > 1) {
       console.log("spm: '" + val + "' is not a spm command. See 'spm --help'.");
       process.exit(1);
     }
   });
-}
-else {
-  slow = false;
+
+  callback(limit);
 }
 
 // simulate PMC an SPM data
@@ -73,26 +58,26 @@ sunseed_parser.pmc(data_pmc, function (err, parsed_data) {
   }
 });
 
-fs.readFile('/etc/machine-id', function (err, file_data) {
-  if (err) {
-    return console.log(err);
-  }
-  machine_id = file_data.toString().slice(0, -1);
-  sunseed_parser.spm(data_spm, machine_id, function (err, parsed_data) {
+process_cli(function (limit) {
+  fs.readFile('/etc/machine-id', function (err, file_data) {
     if (err) {
-      console.log(err + " Data: " + data_spm);
+      return console.log(err);
     }
-    else {
-      if (typeof slow !== 'undefined') {
-        if (slow) {
-          if (parsed_data.indexOf('"report_n":49') !== -1) {
+    machine_id = file_data.toString().slice(0, -1);
+    sunseed_parser.spm(data_spm, machine_id, function (err, parsed_data) {
+      if (err) {
+        console.log(err + " Data: " + data_spm);
+      }
+      else {
+        if (limit) {
+          if (parsed_data.indexOf('"report_n":2') !== -1) {
             console.log("\nSPM data: " + parsed_data);
           }
         } else {
           console.log("\nSPM data: " + parsed_data);
         }
       }
-    }
+    });
   });
 });
 
