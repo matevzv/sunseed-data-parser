3013aa100500ab764077d0a27b5d4560be7af19e
Matevz Vucnik <vucnikm@gmail.com>
2016-11-18 16:27:21 +0100

    add pmc toggle example

---
+++ pmc-toggle.js
@@ -0,0 +1,22 @@
+var Port = require('serialport');
+var parser = require('sunseed-parser');
+
+var port = new Port('/dev/ttyMFD1', {
+  baudRate: 115200,
+  parity: 'odd'
+});
+
+port.on('data', function (data) {
+  parser.pmc(data, function (err, parsed_data) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(parsed_data);
+      parser.toggle([0, 0 ,0], function (err, message) {
+        if (err) return console.log(err.message);
+        port.write(message);
+      });
+    }
+  });
+});
