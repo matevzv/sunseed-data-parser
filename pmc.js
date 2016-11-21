e69d2e3e00acd2812ef89651d52cdfc7cd0506ca
Matevz Vucnik <vucnikm@gmail.com>
2016-11-21 10:58:12 +0100

    add pmc toggle file support

--- pmc.js
+++ pmc.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env nodejs
 
 var Port = require('serialport');
-var parser = require('sunseed-parser');
+var sunseed_parser = require('sunseed-parser');
 
 var port = new Port('/dev/ttyMFD1', {
   baudRate: 115200,
@@ -9,12 +9,16 @@ var port = new Port('/dev/ttyMFD1', {
 });
 
 port.on('data', function (data) {
-  parser.pmc(data, function (err, parsed_data) {
+  sunseed_parser.pmc(data, function (err, parsed_data) {
     if (err) {
       console.log(err);
     }
     else {
-      console.log(parsed_data);
+      var toogle = fs.readFileSync('/var/pmc/toggle', 'utf8');
+      parser.toggle(toogle.split(','), function (err, message) {
+        if (err) return console.log(err.message);
+        port.write(message);
+      });
     }
   });
 });
