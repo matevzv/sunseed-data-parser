1012eaca340c6c87439ed600681cffe6fd14efe9
Matevz Vucnik <vucnikm@gmail.com>
2016-12-01 11:23:26 +0100

    make gto redistributable

--- gto-publish.js
+++ gto-publish.js
@@ -1,27 +1,33 @@
+var fs = require('fs');
 var SerialPort = require('serialport');
 var sunseed_parser = require('sunseed-parser');
-var machine_id = fs.readFileSync('/etc/machine-id', 'utf8').trim();
 
-if (topic == "spm") {
-  wams = sunseed_parser.spm;
-}
-else if (topic == "pmc") {
-  wams = sunseed_parser.pmc_simple;
-}
+var publish = function (client, topic) {
+  var machine_id = fs.readFileSync('/etc/machine-id', 'utf8').trim();
 
-var port = new SerialPort('/dev/ttyMFD1', {
-  baudRate: 115200,
-  parity: 'odd',
-  parser: SerialPort.parsers.readline()
-});
+  if (topic == "spm") {
+    wams = sunseed_parser.spm;
+  }
+  else if (topic == "pmc") {
+    wams = sunseed_parser.pmc_simple;
+  }
 
-port.on('data', function (serial_data) {
-  wams(serial_data, machine_id, function (err, parsed_data) {
-    if (err) {
-      console.log(err + " Data: " + serial_data);
-    }
-    else {
-      client.publish(topic + "/" + machine_id, parsed_data);
-    }
+  var port = new SerialPort('/dev/ttyMFD1', {
+    baudRate: 115200,
+    parity: 'odd',
+    parser: SerialPort.parsers.readline()
   });
-});
+
+  port.on('data', function (serial_data) {
+    wams(serial_data, machine_id, function (err, parsed_data) {
+      if (err) {
+        console.log(err + " Data: " + serial_data);
+      }
+      else {
+        client.publish(topic + "/" + machine_id, parsed_data);
+      }
+    });
+  });
+}
+
+exports.publish = publish;
