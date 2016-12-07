b8868fccd7734809df98acea3d31b1935085a2c0
Matevz Vucnik <vucnikm@gmail.com>
2016-12-07 15:19:12 +0100

    add separate serial settings for spm and pmc

--- gto-publish.js
+++ gto-publish.js
@@ -5,18 +5,20 @@ var sunseed_parser = require('sunseed-parser');
 var publish = function (client, topic) {
   var machine_id = fs.readFileSync('/etc/machine-id', 'utf8').trim();
 
+  var serial_settings = {
+    baudRate: 115200,
+    parity: 'odd'
+  };
+
   if (topic == "spm") {
+    serial_settings.parser = SerialPort.parsers.readline();
     wams = sunseed_parser.spm;
   }
   else if (topic == "pmc") {
     wams = sunseed_parser.pmc_simple;
   }
 
-  var port = new SerialPort('/dev/ttyMFD1', {
-    baudRate: 115200,
-    parity: 'odd',
-    parser: SerialPort.parsers.readline()
-  });
+  var port = new SerialPort('/dev/ttyMFD1', serial_settings);
 
   port.on('data', function (serial_data) {
     wams(serial_data, machine_id, function (err, parsed_data) {
