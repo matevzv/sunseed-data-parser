046ef6dc0d4b01e6a8b043e353d1889bbcf2543e
Matevz Vucnik <vucnikm@gmail.com>
2017-06-05 18:25:16 +0200

    fix pmc-modbus

--- pmc-modbus.js
+++ pmc-modbus.js
@@ -1,12 +1,12 @@
-var pmc_modbus = function (interval, callback) {
-  var ModbusRTU = require("modbus-serial");
-  var client = new ModbusRTU();
+var ModbusRTU = require("modbus-serial");
+var client = new ModbusRTU();
 
-  client.connectRTUBuffered("/dev/ttyMFD1", { baudrate: 9600,
-                                              parity: 'odd'
-                                            });
-  client.setID(10);
+client.connectRTUBuffered("/dev/ttyMFD1", { baudrate: 9600,
+                                            parity: 'odd'
+                                          });
+client.setID(10);
 
+var get_data = function (interval, callback) {
   setInterval(function() {
     client.readInputRegisters(30000, 50, function(err, data) {
       pmc = []
@@ -24,4 +24,4 @@ var pmc_modbus = function (interval, callback) {
   }, interval);
 }
 
-exports.pmc_modbus = pmc_modbus;
+exports.get_data = get_data;
