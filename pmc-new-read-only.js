e7008bdc124d9386535d173a51625b21e2f90118
Matevz Vucnik <vucnikm@gmail.com>
2018-11-29 15:07:37 +0100

    add pmc new read only

---
+++ pmc-new-read-only.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var mqtt = require('mqtt');
+var SerialPort = require('serialport');
+var sunseed_parser = require('sunseed-parser');
+
+var machine_id = fs.readFileSync('/etc/machine-id', 'utf8').trim();
+var topic = "pmc/" + machine_id;
+
+var url = 'mqtt://localhost'
+var client  = mqtt.connect(url);
+client.on('connect', function () {
+    console.log("Connected to " + url)
+});
+
+var port = new SerialPort('/dev/ttyMFD1', {
+    baudRate: 115200,
+    parity: 'odd'
+});
+
+var parsers = SerialPort.parsers;
+var parser = new parsers.Readline();
+port.pipe(parser);
+
+parser.on('data', function (serial_data) {
+    sunseed_parser.pmc_simple(serial_data, machine_id, function (err, parsed_data) {
+        if (err) {
+            console.log(err + " Data: " + serial_data);
+        }
+        else {
+            client.publish(topic, parsed_data);
+        }
+    });
+});
