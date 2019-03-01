fb53c630f0a5d9a690f467a0932c8a8a991149c6
Matevz Vucnik <vucnikm@gmail.com>
2019-03-01 17:36:00 +0100

    add websocket support

---
+++ pmc-new-read-only-wss.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var mqtt = require('mqtt');
+var SerialPort = require('serialport');
+var sunseed_parser = require('sunseed-parser');
+
+var machine_id = fs.readFileSync('/etc/machine-id', 'utf8').trim();
+var topic = "pmc/" + machine_id;
+
+var url = "wss://example.com/mqtt"
+
+var client = mqtt.connect(url, {
+    username: "user",
+    password: "secret"
+});
+
+client.on('connect', function () {
+    console.log("Connected to " + url)
+});
+
+var port = new SerialPort('/dev/ttyMFD1', {
+    baudRate: 115200,
+    parity: 'odd'
+});
+
+var parser = port.pipe(new SerialPort.parsers.Readline());
+
+parser.on('data', function (serial_data) {
+    sunseed_parser.pmc_simple(serial_data, machine_id, function (err, parsed_data) {
+        if (err) {
+            console.log(err + " Data: " + serial_data);
+        }
+        else {
+            client.publish(topic, parsed_data);
+        }
+    });
+});
